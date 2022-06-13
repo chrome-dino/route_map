b141755dc6105efb86272214b6b29a9bb06dde0e
chrome-dino <42478360+chrome-dino@users.noreply.github.com>
2022-06-13 19:47:56 -0400

    Update __main__.py

--- src/route_map/__main__.py
+++ src/route_map/__main__.py
@@ -1,38 +1,21 @@
 import argparse
-import route_mao
+import route_map
 
 
 def main():
     parser = argparse.ArgumentParser()
 
     # cmd line args
-    valid = ''
-    for x in os_whitelist:
-        valid += "'" + x + "',"
-    valid = valid[:-1]
-    parser.add_argument("-f", "--host_file", help="Name of the host file containing line seperated list of IPs or FQDNs. Defaults to hosts.txt",required=False)
-    parser.add_argument("-o", "--operating_systems", help="Comma seperated list of operating systems. Valid OSs are as follows: " + valid,required=True)
-
-
-    
+    parser.add_argument("-f", "--file", help="Path to input pcap file",required=False)
+    parser.add_argument("-f", "--file", help="Path to input pcap file",required=False)
+    parser.add_argument("-o", "--output", help="Path to output kml file. Defaults to route_map.kml",required=False)
+    parser.add_argument("-p", "--public", help="Define public IP. Leave blank to auto retrieve.",required=False)
+    parser.add_argument("-l", "--local", help="Define local IP. Leave blank to auto retrieve.",required=False)
     
     args = parser.parse_args()
 
-    os_list = args.operating_systems.split(',')
-    os_final = []
-    
-    # catch invalid OS entries
-    for x in os_list:
-        if x.strip() not in os_whitelist:
-            print(x.strip() + ' is an invalid OS. Valid OSs are as follows:')
-            for y in os_whitelist:
-                print(y)
-            exit(-1)
-        else:
-            os_final.append(x.strip())
-
-    audit = patch_compliance.PatchCompliance(os_list=os_final,host_file=args.host_file)
-    audit.run()
+    route_map_obj = route_map.RouteMap(os_list=os_final,host_file=args.host_file)
+    route_map_obj.run()
     
 if __name__ == "__main__":
     main()
