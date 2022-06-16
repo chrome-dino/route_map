02f1563842f7caadb8dcbae1d033ed9ea38ca0f2
chrome-dino <42478360+chrome-dino@users.noreply.github.com>
2022-06-15 20:19:13 -0400

    Update __main__.py

--- src/route_map/__main__.py
+++ src/route_map/__main__.py
@@ -4,18 +4,16 @@ import route_map
 
 def main():
     parser = argparse.ArgumentParser()
-
+    group = parser.add_mutually_exclusive_group(required=True)
     # cmd line args
-    parser.add_argument("-f", "--file", help="Path to input pcap file.",required=False)
-    parser.add_argument("-t", "--trace", help="Endpoint to trace to.",required=False)
+    group.add_argument("-f", "--file", help="Path to input pcap file.",required=False)
+    group.add_argument("-t", "--trace", help="Endpoint to trace to.",required=False)
     parser.add_argument("-o", "--output", help="Path to output kml file. Defaults to route_map.kml.",required=False)
     parser.add_argument("-p", "--public", help="Define public IP. Leave blank to auto retrieve.",required=False)
     parser.add_argument("-l", "--local", help="Define local IP. Leave blank to auto retrieve.",required=False)
     
     args = parser.parse_args()
-    if not args.file and not args.trace:
-        print('Requires either a pcap file input (-f) or a endpoint to trace to (-t)')
-        exit(-1)
+
     route_map_obj = route_map.RouteMap(pcap=args.file,trace=args.trace,out=args.output,public=args.public,local=args.local)
     route_map_obj.run()
     
